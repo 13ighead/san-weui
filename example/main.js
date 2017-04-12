3b9aa63df1bce04d5db8558da19ef31fceb1a6d4
lixijun <lixijun@baidu.com>
2017-04-12 11:15:34 +0800

    update san

--- example/main.js
+++ example/main.js
@@ -7,10 +7,34 @@ require('weui')
 
 // route
 import ButtonView from './view/button'
+import ListView from './view/list'
+import CellView from './view/cell'
+import IndexView from './view/index'
 
-import {router} from 'san-router'
+import {
+    router
+} from 'san-router'
 
-router.add({rule: '/', Component: ButtonView, target: '#app'});
+router.add({
+    rule: '/',
+    Component: IndexView,
+    target: '#app'
+});
+router.add({
+    rule: '/button',
+    Component: ButtonView,
+    target: '#app'
+});
+router.add({
+    rule: '/list',
+    Component: ListView,
+    target: '#app'
+});
+router.add({
+    rule: '/cell',
+    Component: CellView,
+    target: '#app'
+});
 
 // start
 router.start()
